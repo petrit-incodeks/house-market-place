600cee2aacad69eef23012c131e179ec3fd998d2
agon-halili <ah41416@ubt-uni.net>
2022-09-15 19:59:09 +0200

    Taking care of console log warnings

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAMVM3Odyw6DURJ3ymTmm4mrKeKsQKwzm4",
-  authDomain: "house-marketplace-app-4f60c.firebaseapp.com",
-  projectId: "house-marketplace-app-4f60c",
-  storageBucket: "house-marketplace-app-4f60c.appspot.com",
-  messagingSenderId: "549254846160",
-  appId: "1:549254846160:web:c5439216613f49b64cb0d5",
+  apiKey: 'AIzaSyAMVM3Odyw6DURJ3ymTmm4mrKeKsQKwzm4',
+  authDomain: 'house-marketplace-app-4f60c.firebaseapp.com',
+  projectId: 'house-marketplace-app-4f60c',
+  storageBucket: 'house-marketplace-app-4f60c.appspot.com',
+  messagingSenderId: '549254846160',
+  appId: '1:549254846160:web:c5439216613f49b64cb0d5',
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 export const db = getFirestore();
